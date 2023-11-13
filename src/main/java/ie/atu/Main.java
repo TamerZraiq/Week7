84bec64cd32fcb9bb082a13beee9ffa2dc913db6
Tamer Zraiq <G00425053@atu.ie>
2023-11-13 16:25:38 +0000

    Added feature1 branch and code

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -3,6 +3,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println("Please enter a number between 0 - 9:");
+        Scanner myscan = new Scanner(System.in);
+
+        //read user input as a sting
+        String input = myscan.nextLine();
+
+        //Parse the input to an integer
+        int myNum= Integer.parseInt(input);
+
+        //check if the number is between 0 and 9
+        if (myNum>= 0 && myNum<=9){
+            System.out.println("You entered " + myNum);
+        }
+        else{
+            System.out.println("Not a valid number");
+        }
     }
 }
